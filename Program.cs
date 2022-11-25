f3eae9262195d223e0ba02719338863aa7ce5b23
SinkevichV <sinkevichvasiliu@mail.ru>
2022-11-25 12:22:20 +0700

    метод для печати массива

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿string[] arrayFirst = new string[8] {"opa", "go", "C#", "dotnet", "155", "sdfjwsoig", "afd", "qw"};
+﻿string[] arrayFirst = new string[8] { "opa", "go", "C#", "dotnet", "155", "sdfjwsoig", "afd", "qw" };
 string[] arraySecond = new string[arrayFirst.Length];
 void SecondArray(string[] arrayFirst, string[] arraySecond)
 {
     int count = 0;
     for (int i = 0; i < arrayFirst.Length; i++)
     {
-    if (arrayFirst[i].Length <= 3)
+        if (arrayFirst[i].Length <= 3)
         {
-        arraySecond[count] = arrayFirst[i];
-        count++;
+            arraySecond[count] = arrayFirst[i];
+            count++;
         }
     }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
