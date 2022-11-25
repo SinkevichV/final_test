551f485d486ed96cf6e15f19dcbdc096f1541009
SinkevichV <sinkevichvasiliu@mail.ru>
2022-11-25 12:20:07 +0700

    метод для получения массива с элементами <=3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] arrayFirst = new string[8] {"opa", "go", "C#", "dotnet", "155", "sdfjwsoig", "afd", "qw"};
+string[] arraySecond = new string[arrayFirst.Length];
+void SecondArray(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+    if (arrayFirst[i].Length <= 3)
+        {
+        arraySecond[count] = arrayFirst[i];
+        count++;
+        }
+    }
+}
+
